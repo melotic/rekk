516d8792debcdfa6b99238bb28b9133e1234b873
Justin Perez <justinmp@vt.edu>
2021-04-03 13:06:41 -0400

    Polish windows runtime.

--- runtime/src/windows_runtime.rs
+++ runtime/src/windows_runtime.rs
@@ -1,80 +1,75 @@
+use std::ffi::CString;
+use std::ptr::null_mut;
 use std::{error, mem};
 
-use common::jump_data_table::JumpDataTable;
-use goblin::pe::PE;
-use goblin::Object;
-use ntapi::ntmmapi::NtUnmapViewOfSection;
 use ntapi::ntpebteb::PEB;
 use ntapi::ntpsapi::{
     NtQueryInformationProcess, ProcessBasicInformation, PROCESS_BASIC_INFORMATION,
 };
 use snap::raw::Decoder;
-use std::alloc::handle_alloc_error;
-use std::ffi::CString;
-use std::ptr::null_mut;
 use winapi::_core::ffi::c_void;
-use winapi::shared::minwindef::{DWORD, FALSE, MAX_PATH, PROC, TRUE};
+use winapi::shared::minwindef::{DWORD, MAX_PATH, TRUE};
 use winapi::shared::ntdef::HANDLE;
 use winapi::shared::winerror::SUCCEEDED;
 use winapi::um::debugapi::{ContinueDebugEvent, WaitForDebugEvent};
-use winapi::um::errhandlingapi::GetLastError;
 use winapi::um::fileapi::{
-    CreateFileA, DeleteFileA, GetTempFileNameA, GetTempPathA, WriteFile, CREATE_ALWAYS, OPEN_ALWAYS,
+    CreateFileA, DeleteFileA, GetTempFileNameA, GetTempPathA, WriteFile, CREATE_ALWAYS,
 };
 use winapi::um::handleapi::CloseHandle;
-use winapi::um::memoryapi::{ReadProcessMemory, VirtualAllocEx, WriteProcessMemory};
+use winapi::um::memoryapi::ReadProcessMemory;
 use winapi::um::minwinbase::DEBUG_EVENT;
 use winapi::um::processenv::GetCommandLineA;
 use winapi::um::processthreadsapi::{
     CreateProcessA, GetThreadContext, OpenThread, ResumeThread, SetThreadContext, SuspendThread,
-    TerminateProcess, PROCESS_INFORMATION, STARTUPINFOA,
+    PROCESS_INFORMATION, STARTUPINFOA,
 };
 use winapi::um::synchapi::WaitForSingleObject;
-use winapi::um::winbase::{CREATE_SUSPENDED, DEBUG_PROCESS, FILE_FLAG_DELETE_ON_CLOSE, INFINITE};
+use winapi::um::winbase::{DEBUG_PROCESS, INFINITE};
 use winapi::um::winnt::{
-    CONTEXT, CONTEXT_CONTROL, CONTEXT_INTEGER, DBG_CONTINUE, FILE_ATTRIBUTE_NORMAL,
-    FILE_SHARE_READ, FILE_SHARE_WRITE, GENERIC_READ, GENERIC_WRITE, MEM_COMMIT, MEM_RESERVE,
-    PAGE_EXECUTE_READWRITE, THREAD_ALL_ACCESS, THREAD_GET_CONTEXT, THREAD_SET_CONTEXT,
+    CONTEXT, CONTEXT_CONTROL, DBG_CONTINUE, FILE_ATTRIBUTE_NORMAL, FILE_SHARE_READ,
+    FILE_SHARE_WRITE, GENERIC_WRITE, THREAD_ALL_ACCESS,
 };
+
+use common::jump_data_table::JumpDataTable;
 
 pub fn run() {
     unsafe {
         //run_binary();
-        let result = run_binary_2().unwrap();
-        println!("{}", result.1);
+        let result = run_binary().unwrap();
         run_handler(result.0, result.1);
     }
 }
 
 unsafe fn run_handler(proc_info: PROCESS_INFORMATION, proc_name: String) {
-    // currently the process is suspended.
-    ResumeThread(proc_info.hThread);
-
     let mut debug_event = mem::zeroed::<DEBUG_EVENT>();
-    let mut continue_status = DBG_CONTINUE;
 
+    // Calculate the address the image was loaded into.
     let base_addr = read_remote_peb(proc_info.hProcess).ImageBaseAddress as u64;
 
-    let raw_jdt = include_bytes!("../../jdt.bin");
+    // Unpack the JDT
     let mut decoder = Decoder::new();
-    let unpacked = decoder.decompress_vec(raw_jdt).unwrap();
+    let unpacked = decoder
+        .decompress_vec(include_bytes!("../../jdt.bin"))
+        .unwrap();
     let jdt: JumpDataTable = bincode::deserialize(unpacked.as_slice()).unwrap();
 
     loop {
         WaitForDebugEvent(&mut debug_event, INFINITE);
 
-        //println!("DebugEvent: {}", debug_event.dwDebugEventCode);
+        if debug_event.dwDebugEventCode != 1 {
+            println!("DebugEvent: {}", debug_event.dwDebugEventCode);
+        }
 
         match debug_event.dwDebugEventCode {
-            1 => handle_int3(
-                &mut continue_status,
-                debug_event.dwThreadId,
-                base_addr,
-                &jdt,
-            ),
+            // Received an exception.
+            1 => handle_int3(debug_event.dwThreadId, base_addr, &jdt),
+            // The process has exited.
             5 => {
-                println!("exit: {}", debug_event.u.ExitProcess().dwExitCode);
                 break;
+            }
+            // Something no bueno happened.
+            0 => {
+                panic!("error waiting for debug event.");
             }
             _ => {}
         }
@@ -82,15 +77,33 @@ unsafe fn run_handler(proc_info: PROCESS_INFORMATION, proc_name: String) {
         ContinueDebugEvent(
             debug_event.dwProcessId,
             debug_event.dwThreadId,
-            continue_status,
+            DBG_CONTINUE,
         );
     }
+
+    // Let the process terminate.
+    ContinueDebugEvent(
+        debug_event.dwProcessId,
+        debug_event.dwThreadId,
+        DBG_CONTINUE,
+    );
+
+    // Wait for the process to terminate.
+    WaitForSingleObject(proc_info.hProcess, INFINITE);
+
+    // Free handles
+    CloseHandle(proc_info.hProcess);
+    CloseHandle(proc_info.hThread);
+
+    // Delete the dropped file.
+    DeleteFileA(proc_name.as_ptr() as *const _);
 }
 
 unsafe fn read_remote_peb(proc_handle: HANDLE) -> PEB {
     let mut pbi = mem::zeroed::<PROCESS_BASIC_INFORMATION>();
     let mut written = 0;
 
+    // Get the ProcessBasicInformation to locate the address of the PEB.
     NtQueryInformationProcess(
         proc_handle,
         ProcessBasicInformation,
@@ -100,8 +113,9 @@ unsafe fn read_remote_peb(proc_handle: HANDLE) -> PEB {
     );
 
     let mut peb = mem::zeroed::<PEB>();
-
     let mut written = 0;
+
+    // Read the PEB.
     ReadProcessMemory(
         proc_handle,
         pbi.PebBaseAddress as *const _,
@@ -113,12 +127,7 @@ unsafe fn read_remote_peb(proc_handle: HANDLE) -> PEB {
     peb
 }
 
-unsafe fn handle_int3(
-    continue_status: &mut u32,
-    thread_id: DWORD,
-    base_addr: u64,
-    jdt: &JumpDataTable,
-) {
+unsafe fn handle_int3(thread_id: DWORD, base_addr: u64, jdt: &JumpDataTable) {
     // Open a handle to the thread.
     let handle = OpenThread(THREAD_ALL_ACCESS, TRUE, thread_id);
 
@@ -126,12 +135,18 @@ unsafe fn handle_int3(
         panic!("couldn't access thread.");
     }
 
-    let ret = SuspendThread(handle);
-    let mut context = mem::zeroed::<CONTEXT>();
-    context.ContextFlags = CONTEXT_CONTROL;
+    // GetThreadContext requires the thread is suspended. It should already be suspended, this is for redundancy.
+    SuspendThread(handle);
 
+    let mut context = mem::zeroed::<CONTEXT>();
+    context.ContextFlags = CONTEXT_CONTROL; // We only need RIP and EFLAGS.
+
+    // Get the thread context.
     let ret = GetThreadContext(handle, &mut context);
 
+    // LdrpInitializeProcess will trigger a breakpoint if the process is being debugged when it is
+    // created. This function will handle that breakpoint, but GetThreadContext only allows us to
+    // get the context of threads we own. Thus, if GetThreadContext fails we should just move on.
     if ret == 0 {
         ResumeThread(handle);
         CloseHandle(handle);
@@ -140,6 +155,8 @@ unsafe fn handle_int3(
 
     let jump_data = jdt.get_jump_data(context.Rip - 1 - base_addr);
 
+    // If there was an error getting the jump data, jump to the next instruction and hope for the
+    // best.
     if jump_data.is_err() {
         context.Rip += 1;
         SetThreadContext(handle, &context);
@@ -150,29 +167,30 @@ unsafe fn handle_int3(
 
     let jump_data = jump_data.unwrap();
 
+    // Add the signed offset to RIP.
     let offset = jump_data.get_ip_offset(context.EFlags as u64);
     context.Rip = (context.Rip as i64 + offset as i64 - 1) as u64;
 
+    // Update RIP, resume the thread, and get rid of our handle.
     SetThreadContext(handle, &context);
     ResumeThread(handle);
-
     CloseHandle(handle);
 }
 
-unsafe fn run_binary_2() -> Result<(PROCESS_INFORMATION, String), Box<dyn error::Error>> {
-    //let binary = include_bytes!("../../test/test.exe");
+unsafe fn run_binary() -> Result<(PROCESS_INFORMATION, String), Box<dyn error::Error>> {
     let binary = include_bytes!("../../nanomite.bin");
 
     // decompress the binary.
     let mut decoder = Decoder::new();
     let d_bin = decoder.decompress_vec(binary)?;
-    //let d_bin = binary;
 
+    // Get the path to %temp%.
     let mut buf: [u8; MAX_PATH] = [0; MAX_PATH];
     GetTempPathA(MAX_PATH as u32, buf.as_mut_ptr() as *mut _);
 
     let prefix = CString::new("").unwrap();
 
+    // Get a random file name in that directory.
     let mut temp_file_name_buf: [u8; MAX_PATH] = [0; MAX_PATH];
     GetTempFileNameA(
         buf.as_mut_ptr() as *mut _,
@@ -181,6 +199,7 @@ unsafe fn run_binary_2() -> Result<(PROCESS_INFORMATION, String), Box<dyn error:
         temp_file_name_buf.as_mut_ptr() as *mut _,
     );
 
+    // Create a file with the generated name to write to.
     let h_file = CreateFileA(
         temp_file_name_buf.as_ptr() as *mut _,
         GENERIC_WRITE,
@@ -191,6 +210,7 @@ unsafe fn run_binary_2() -> Result<(PROCESS_INFORMATION, String), Box<dyn error:
         null_mut(),
     );
 
+    // Write the nanomite'd binary to the new file.
     let mut written = 0;
     WriteFile(
         h_file,
@@ -200,9 +220,10 @@ unsafe fn run_binary_2() -> Result<(PROCESS_INFORMATION, String), Box<dyn error:
         null_mut(),
     );
 
+    // Flush the changes.
     CloseHandle(h_file);
 
-    // create process
+    // Create the new process, and debug it.
     let mut startup_info = mem::zeroed::<STARTUPINFOA>();
     let mut process_info = mem::zeroed::<PROCESS_INFORMATION>();
 
@@ -217,16 +238,6 @@ unsafe fn run_binary_2() -> Result<(PROCESS_INFORMATION, String), Box<dyn error:
         null_mut(),
         &mut startup_info,
         &mut process_info,
-    );
-
-    let h_file = CreateFileA(
-        temp_file_name_buf.as_ptr() as *const _,
-        GENERIC_READ,
-        0,
-        null_mut(),
-        OPEN_ALWAYS,
-        FILE_ATTRIBUTE_NORMAL | FILE_FLAG_DELETE_ON_CLOSE,
-        null_mut(),
     );
 
     Ok((
